dabc97af1c72934c921413019f34435fb60fe5b5
Maxim <71179917+Postmaxxx@users.noreply.github.com>
2022-09-18 18:05:10 +0300

    new splide created

--- src/assets/js/about_us.js
+++ src/assets/js/about_us.js
@@ -2,103 +2,50 @@ import {makeCarousel} from './components/carousel-max.js';
 import {makeSideNav} from './components/nav_side.js';
 
 
-
-let carousel_prop = {
-    destinationClass : 'homepage__hero__carousel_1',
-    imgWidth : 600,
-    imgGap : 15,
-    bgMoveSpeed : 5, //moving without dragging, defaultSpeed, px/tick
-    timeToBgMove : 5000, //time to start background movement after stop in ms
-    inertiaSensivity : 10, //inetria turns on when moving speed is higher
-    inertiaStep : 0.99, //currentInertiaSpeed = inertiaStep * currentInertiaSpeed every tick
-    imagesPaths : [['./assets/img/1.jpg', 'First project of our organisation  &#8594', 'http://twitter.com'],
-                    ['./assets/img/2.jpg', 'Second project &#8594', 'http://yahoo.com'],
-                    ['./assets/img/3.jpg', 'Project 3 &#8594', 'http://mail.com'],
-                    ['./assets/img/4.jpg', 'Project 4 &#8594', 'http://hotmail.com'],
-                    ['./assets/img/5.jpg', 'Project 5 &#8594', 'http://p2sinc.com'],
-                    ['./assets/img/6.jpg', 'Project 6 &#8594', 'http://youtube.com']],
-    expandPath : './assets/json/Plus_Widget.json',
-    closePath : './assets/img/x.png',
-    nodeForFullsize : 'content-container',
-    expandIconWidth: 70,
-    expandIconHeignt: 70,
-    transitionTime: 1
-}
-let carousel = makeCarousel(carousel_prop);
-
 makeSideNav();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  let iconMenu1 = document.querySelector('.homepage__our-divisions__item__logo_eng');
-  let animationMenu1 = bodymovin.loadAnimation({
-          container: iconMenu1,
-          renderer: 'svg',
-          loop: true,
-          autoplay: false,
-          path: "./assets/json/ENG_Widget.json",
-          initialSegment: [34, 34],
+document.addEventListener( 'DOMContentLoaded', function() {
+  var splideAboutUs_1 = new Splide('.splide_about-us_1', {
+      type: "loop",
+      perPage: 1,
+      clones:1, 
+      arrows: false,
+      drag: true,
+      dragMinThreshold: {
+          mouse: 0,
+          touch: 10,
+      },
+      autoplay: false,
+      interval: 4000,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      cover: true,
+      heightRatio: .5,
+      gap: 20,
+      padding: { right: "15%"},
+      arrows: true,
+      classes: {
+          pagination: 'splide__pagination_text',
+          page      : 'splide__pagination_text__pagination',
+      },
   });
+
+  const splideLabels = Array.from(document.querySelector(".splide_about-us_1").querySelectorAll("[data-descr]"))
+    .map(slide => slide.dataset.descr)
+
   
-  iconMenu1.addEventListener('mouseenter', (e) => {
-    animationMenu1.playSegments([0, 34], true);
-  });
-  iconMenu1.addEventListener('mouseleave', (e) => {
-    animationMenu1.goToAndStop(34, 34)
-  });
+  splideAboutUs_1.on( 'pagination:mounted', data => {
+  // `items` contains all dot items
+      data.items.forEach((item, index) => {
+          item.button.textContent = String( splideLabels[index] );
+      } );
+  } );
 
 
+  splideAboutUs_1.mount();
 
-
-
-  let iconMenu2 = document.querySelector('.homepage__our-divisions__item__logo_cx');
-  let animationMenu2 = bodymovin.loadAnimation({
-          container: iconMenu2,
-          renderer: 'svg',
-          loop: true,
-          autoplay: false,
-          path: "./assets/json/Cx Widget.json",
-          initialSegment: [54, 97],
-  });
-  
-  iconMenu2.addEventListener('mouseenter', (e) => {
-    animationMenu2.playSegments([0, 97], true);
-  });
-
-  iconMenu2.addEventListener('mouseleave', (e) => {
-    animationMenu2.goToAndStop(54, 97)
-  });
-
-
-
-
-
-
-  let iconMenu3 = document.querySelector('.homepage__our-divisions__item__logo_cm');
-  let animationMenu3 = bodymovin.loadAnimation({
-          container: iconMenu3,
-          renderer: 'svg',
-          loop: true,
-          autoplay: false,
-          path: "./assets/json/CM_Widget.json",
-          initialSegment: [34, 55],
-  });
-  
-  iconMenu3.addEventListener('mouseenter', (e) => {
-    animationMenu3.playSegments([0, 55], true);
-  });
-
-  iconMenu3.addEventListener('mouseleave', (e) => {
-    animationMenu3.goToAndStop(34, 55)
-  });
+    //move pagination between arrows
+    document.querySelector(".splide_about-us_1").querySelector(".splide__arrows").firstElementChild
+    .after(document.querySelector(".splide_about-us_1").querySelector(".splide__pagination_text"))
+});
